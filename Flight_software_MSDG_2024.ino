74f7392f12e836c06ae7b1efacecfda7e411d5e9
winterbottomelliot@gmail.com <winterbottomelliot@gmail.com>
2024-03-27 18:03:10 +0000

    Add project files.

---
+++ Flight_software_MSDG_2024.ino
@@ -0,0 +1,22 @@
+/*
+ Name:		Flight_software_MSDG_2024.ino
+ Created:	3/27/2024 5:22:23 PM
+ Author:	Elliot winterbottom, PUT YOUR NAME HERE!!
+
+ This is the main flight software code for the manchester cansat project 2024. 
+ +it runs on the Visual Micro extension for Visual studio
+ +it is set to upload onto the Teensy 4.0 board 
+ + Follow this guide to install the correct drivers https://www.youtube.com/watch?v=RcEzUjNr634
+
+*/
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
