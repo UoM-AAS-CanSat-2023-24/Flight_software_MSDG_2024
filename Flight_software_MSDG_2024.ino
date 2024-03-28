d19e575e7664f8a3d9f7fde361c1c1488cd4e7c9
winterbottomelliot@gmail.com <winterbottomelliot@gmail.com>
2024-03-28 02:43:58 +0000

    + added extra variables for sate control +added timing variables + added constants + added function prototypes +extra case statement

--- Flight_software_MSDG_2024.ino
+++ Flight_software_MSDG_2024.ino
@@ -13,26 +13,67 @@
 	+ALLCAPS is used for constants
 	+PascalCase is used for classes
 	+snake_case is used for functions excluding the setup and loop functions.
+
+	Current problems to solve:
+	- Which time to be used for which mechanisms?
+	- pulling data from sensors (Ask Emily)
+	- pushing data to telemetry (ask Matej)
+	-
  
 */
 
 
 //---constants---
+#define SERVO_PIN_CAMERA 0
+#define SERVO_PIN_PARACHUTE 1
+#define SERVO_PIN_AEROBREAK 2
 
+#define FIXED_CAM_PIN 6
+#define PID_CAM_PIN 7
+
+#define BUZZER_PIN 12
+#define LED_PIN 11
 
 //---global variables---
 bool flightSate;// 0 for ascent, 1 for descent
 bool cameraSate;// 0 for inactive, 1 for active
 bool simSate; // 0 for actual flight, 1 for simulation state
 bool groundedSate; // 0 for airborne 1 for grounded 
+bool telemetrySate; // 0 for inactive 1 for active 
+
+long int programTime; // needs to be set in millis or micros depending on what louis says 
+int telemetryTime[3]; // stores the current time sent with any telemetry 
+
+double satelliteAltitude; // this could change to float depending on size of input from sensor 
 
 
 
 //---function prototypes---
 
+void sensor_setup(); // sets up sensors one I2c busses 
+void sensor_poll(bool flightSateArg, bool simSateArg ); // polls sensors based on the state of the satellite 
+
+void telemetry_transmit(); // transmits data 
+void telemetry_receive(); // receive data for sim mode
+
+void EEPROM_data_save(); // saves data from sensors to EEPROM
+void EEPROM_data_load();
+void SD_data_save();
+
+void buzzer_control(); // activates and deactivates the buzzer
+
+void aerobreak_deploy(); // deploys the heat sheild  and parachute respectively 
+void parachute_deploy();
+
+void PID_set(); // sets values for PID loop and will update servo motor control
+
+
+
 
 
 //---function definitions---
+
+
 
 
 // the setup function runs once when you press reset or power the board
@@ -47,9 +88,11 @@ void loop()
 {
 
 
-	switch (flightSate)
+
+	switch (flightSate) // case statements used as they are likely more effecient than if statments for massively reapeted satements 
 	{
 	case 0: // ascending 
+
 
 
 		break;
@@ -70,6 +113,21 @@ void loop()
 
 		break;
 	case 1: // active camera
+
+
+
+		break;
+	}
+
+	switch (telemetrySate)
+	{
+	case 0: // inactive telemetry
+
+
+
+
+		break;
+	case 1: // active telemetry 
 
 
 
